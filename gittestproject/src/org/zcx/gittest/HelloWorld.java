42ed23a3f98d685ee7df75777e1d783443feebdf
zhuchunxi <goodMorning_glb@atguigu.com>
2020-09-17 16:04:57 +0800

    把本地工程添加到git本地仓库

---
+++ gittestproject/src/org/zcx/gittest/HelloWorld.java
@@ -0,0 +1,17 @@
+package org.zcx.gittest;
+
+/**
+ * @author zhuchuxnxi
+ * @create 2020-09-17 15:46
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("git hello");
+        System.out.println("git hello");
+        System.out.println("git hello");
+        System.out.println("git hello");
+        System.out.println("git hello");
+        System.out.println("git hello");
+    }
+}
